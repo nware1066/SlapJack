e02f69f9a65c2434078afff4a8a5317579e6f23a
Naomi Ware <nware1066@yahoo.com>
2020-06-04 17:23:50 -0600

    add player class

---
+++ src/player.js
@@ -0,0 +1,15 @@
+class Player {
+  constructor() {
+    this.id = Date.now();
+    this.wins = 0;
+    this.hand = [];
+  }
+
+  playCard() {
+    // move index[0] from hand to centerPile
+  }
+
+  saveWins(){
+    // add win to local storage
+  }
+}
